8bda2ca220a167de0bb6927047fdd0bc572d28d1
Michel Breyer <michel.p.breyer@gmail.com>
2021-08-13 14:32:14 +0200

    Add line list marker

--- robot_helpers/ros/rviz.py
+++ robot_helpers/ros/rviz.py
@@ -8,6 +8,12 @@ def create_cube_marker(frame, pose, scale, color, ns="", id=0):
     return create_marker(Marker.CUBE, frame, pose, scale, color, ns, id)
 
 
+def create_line_list_marker(frame, pose, scale, color, lines, ns="", id=0):
+    marker = create_marker(Marker.LINE_STRIP, frame, pose, scale, color, ns, id)
+    marker.points = [to_point_msg(point) for line in lines for point in line]
+    return marker
+
+
 def create_line_strip_marker(frame, pose, scale, color, points, ns="", id=0):
     marker = create_marker(Marker.LINE_STRIP, frame, pose, scale, color, ns, id)
     marker.points = [to_point_msg(point) for point in points]
