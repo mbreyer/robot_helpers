169e464ba6d95da60e376ea1fe67ec449a83a9f6
Michel Breyer <10465414+mbreyer@users.noreply.github.com>
2022-02-04 16:26:43 +0100

    Add pickle to io

--- robot_helpers/io.py
+++ robot_helpers/io.py
@@ -1,8 +1,20 @@
-import yaml
 from pathlib import Path
+import pickle
+import yaml
 
 
 def load_yaml(path):
     with Path(path).open("r") as f:
         cfg = yaml.load(f, Loader=yaml.FullLoader)
     return cfg
+
+
+def load_pickle(path):
+    with Path(path).open("rb") as f:
+        data = pickle.load(f)
+    return data
+
+
+def save_pickle(data, path):
+    with Path(path).open("wb") as f:
+        pickle.dump(data, f)
